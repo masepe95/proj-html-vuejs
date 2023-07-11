5eccf4a6d261f58b3a2a5c9d297c9069ef72b883
masepe95 <marcosepe10@gmail.com>
2023-07-11 12:45:30 +0200

    First slide/Newsletter Added - imgPath not working

---
+++ src/components/Main/FirstSlide.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="slide">
+    <div class="container">
+      <div class="text">
+        <h1>
+          Limitless learning,<br />
+          more possibilities
+        </h1>
+        <p>
+          Online courses open the opportunity for learning to almost anyone,
+          regardless of their scheduling commitments.
+        </p>
+        <button type="button" class="btn btn-primary p-3">Learn More</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstSlide",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/sass/style.scss";
+
+.slide {
+  background-image: url("../../assets/img/Untitled-1-1-1-1-1.png");
+  background-color: $backgroundlightblue;
+  background-position: center;
+  padding: 6rem 0;
+
+  .text {
+    width: 40%;
+    @include flex(column, center, flex-start);
+    gap: 1rem;
+
+    p {
+      opacity: 0.7;
+    }
+  }
+}
+
+@include md {
+  .slide {
+    background-image: none;
+
+    .text {
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/components/Main/PageMain.vue
+++ src/components/Main/PageMain.vue
@@ -1,7 +1,49 @@
-<script>
+<template>
+  <div>
+    <JumbotronDiv />
+    <FirstSlide />
+    <CoursesDiv :info="allCourses" />
+    <SecondSlide />
+    <NewsletterDiv />
+    <CoursesDiv :info="popularCourses" />
+    <ThirdSlide />
+    <ReviewDiv />
+  </div>
+</template>
 
+<script>
+import JumbotronDiv from "./Jumbotron.vue";
+import FirstSlide from "./FirstSlide.vue";
+import CoursesDiv from "./Courses.vue";
+import SecondSlide from "./SecondSlide.vue";
+import NewsletterDiv from "./Newsletter.vue";
+import ThirdSlide from "./ThirdSlide.vue";
+import ReviewDiv from "./ReviewDiv.vue";
+
+export default {
+  name: "MainDiv",
+  components: {
+    JumbotronDiv,
+    FirstSlide,
+    CoursesDiv,
+    SecondSlide,
+    NewsletterDiv,
+    ThirdSlide,
+    ReviewDiv,
+  },
+  data() {
+    return {
+      allCourses: {
+        title: "Recent courses",
+        bg: "white",
+      },
+      popularCourses: {
+        title: "Popular courses",
+        bg: "$backgroundlightblue",
+      },
+    };
+  },
+};
 </script>
 
-<template></template>
-
-<style></style>
+<style lang="scss"></style>
